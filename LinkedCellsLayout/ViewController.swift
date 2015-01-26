e5a5a4b9cd35b45c4cd88bcee9d579b581a3c766
Andrew Theken <theken.1@osu.edu>
2015-01-26 13:09:44 -0500

    Initial Commit

---
+++ LinkedCellsLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkedCellsLayout
+//
+//  Created by Andrew Theken on 1/26/15.
+//  Copyright (c) 2015 Andrew Theken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
